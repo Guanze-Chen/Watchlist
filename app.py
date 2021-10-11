f3d6a61314a152fbd453de7cdff4384a93e368c2
Guanze <ScuGuanze@163.com>
2021-10-11 11:45:25 +0800

    Add database support with Flask-SQLALchemy

--- app.py
+++ app.py
@@ -1,7 +1,72 @@
 from flask import Flask
 from flask import escape, url_for
+from flask_sqlalchemy import SQLAlchemy
+import os
+
 
 app = Flask(__name__)
+
+# 数据库配置
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(app.root_path,'data.db')
+
+
+db = SQLAlchemy(app)
+
+# 创建数据库模型
+class User(db.Model): # 表明将会是user(自动生成 小写处理)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+    
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(60))
+    year = db.Column(db.String(4))
+    
+# 自定义命令initdb
+import click
+
+@app.cli.command() # 注册为命令
+@click.option('--drop', is_flag=True, help='Create after drop') # 设置选项
+def initdb(drop):
+    """initialize the database"""
+    if drop:
+        db.drop_all()
+    db.create_all()
+    click.echo('initialized database') # 输出提示信息
+    
+    
+@app.cli.command()
+def forge():
+    """generate fake data"""
+    db.create_all()
+    
+    name = "Fake"
+    movies = [
+        {'title': 'My Neighbor Totoro', 'year': '1988'},
+        {'title': 'Dead Poets Society', 'year': '1989'},
+        {'title': 'A Perfect World', 'year': '1993'},
+        {'title': 'Leon', 'year': '1994'},
+        {'title': 'Mahjong', 'year': '1996'},
+        {'title': 'Swallowtail Butterfly', 'year': '1996'},
+        {'title': 'King of Comedy', 'year': '1999'},
+        {'title': 'Devils on the Doorstep', 'year': '1999'},
+        {'title': 'WALL-E', 'year': '2008'},
+        {'title': 'The Pork of Music', 'year': '2012'},
+    ]
+    
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'],year=m['year'])
+        db.session.add(movie)
+    db.session.commit()
+    click.echo('Done')
+    
+
+
+    
 
 # @app.route('/')
 # @app.route('/home')
@@ -24,20 +89,9 @@ app = Flask(__name__)
 
 from flask import render_template
 
-name = 'Guan Ze'
-movies = [
-    {'title': 'My Neighbor Totoro', 'year': '1988'},
-    {'title': 'Dead Poets Society', 'year': '1989'},
-    {'title': 'A Perfect World', 'year': '1993'},
-    {'title': 'Leon', 'year': '1994'},
-    {'title': 'Mahjong', 'year': '1996'},
-    {'title': 'Swallowtail Butterfly', 'year': '1996'},
-    {'title': 'King of Comedy', 'year': '1999'},
-    {'title': 'Devils on the Doorstep', 'year': '1999'},
-    {'title': 'WALL-E', 'year': '2008'},
-    {'title': 'The Pork of Music', 'year': '2012'},
-]
 
 @app.route('/')
 def index():
-    return render_template('./index.html',name=name,movies=movies)
+    user = User.query.first()
+    movies = Movie.query.all()
+    return render_template('./index.html',user=user,movies=movies)
